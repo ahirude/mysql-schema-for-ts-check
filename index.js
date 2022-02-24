3315a64cc06cdf4158fc9466e532505d9b8b26c9
ahirude <iwa0843@gmail.com>
2022-02-24 18:17:15 +0900

    feat: add -jsdoc

--- index.js
+++ index.js
@@ -2,128 +2,46 @@
 'use strict';
 
 // usage
-//   node index.js mysql://root:passwd@mysql-server.example.com/database [-d destdir]
-//   node index.js mysql://root:passwd@mysql-server.example.com/database/table [-d destdir]
+//   node index.js mysql://root:passwd@mysql-server.example.com/database [-dir destdir] [-jsdoc]
+//   node index.js mysql://root:passwd@mysql-server.example.com/database/table [-dir destdir] [-jsdoc]
 
 const pMap = require('p-map');
 const mysql = require('mysql2/promise');
 const fs = require('fs');
 const commandLineArgs = require('command-line-args');
 
+const makeEnumMap = require('./lib/make-enum-map');
+const listTableName = require('./lib/list-table-name');
+const listColumn = require('./lib/list-column');
+const makeColumnInfo = require('./lib/make-column-info');
+
 /** @type {{dir?:string, src:string}} */ // @ts-ignore
 const options = commandLineArgs([
   {name: 'dir', alias: 'd', type: String},
-  {name: 'src', alias: 's', type: String, defaultOption: true}
+  {name: 'src', alias: 's', type: String, defaultOption: true},
+  {name: 'jsdoc', alias: 'j', type:String}
 ]);
 
-/**
- * @typedef EnumTypeMap
- * @type {Map<string, string>}
- * @description K: "enum('Y','N')", V: enumType01
- */
-const enumToTypeMap = new Map();
-const enumTypeUsed = new Set();
-
-
-/**
- * enum('A','B') -> K:"enum('A','B')", V:"'A' | 'B'"
- * @param {Object} params
- * @param {mysql.Connection} params.conn
- * @param {string} params.database
- * @param {string|null} params.table
- * @param {Array<string>} lines
- */
-const makeEnumMap = async (params, lines) => {
-  const sql = `SELECT DISTINCT COLUMN_TYPE FROM INFORMATION_SCHEMA.COLUMNS where TABLE_SCHEMA = ? AND DATA_TYPE = 'enum'`;
-
-  /** @type {[Array<{COLUMN_TYPE:string}>]} */ // @ts-ignore
-  const [enumList] = await params.conn.query(sql, [params.database]);
-  enumList.forEach((row) => {
-    if (enumToTypeMap.has(row.COLUMN_TYPE))  return;
-    const declare = row.COLUMN_TYPE.replace('enum(', '').replace(')', '').replace(/,/g, ' | '); // "'Y' | 'N'""
-    enumToTypeMap.set(row.COLUMN_TYPE, `string | (${declare})`); // for working with JS on VS-Code
-  });
-};
-
-
-const sqlColumns = `SELECT COLUMN_NAME, IS_NULLABLE, DATA_TYPE, COLUMN_TYPE, COLUMN_KEY, COLUMN_COMMENT FROM COLUMNS
-WHERE TABLE_SCHEMA = ? AND TABLE_NAME = ? ORDER BY ORDINAL_POSITION`;
-/**
- * fields of 'COLUMNS'
- * @typedef ColumnInfo
- * @type {Object}
- * @property {string} COLUMN_NAME
- * @property {string} IS_NULLABLE
- * @property {string} DATA_TYPE
- * @property {string} COLUMN_TYPE
- * @property {string} COLUMN_KEY
- * @property {string} COLUMN_COMMENT
- * @description  ex: {COLUMN_NAME: 'sub_id', IS_NULLABLE: 'NO', DATA_TYPE: 'bigint', COLUMN_TYPE: 'bigint(20)', COLUMN_KEY: '', COLUMN_COMMENT: 'sub ID' }
- */
-/**
- * declare each column
- * @param {ColumnInfo} row
- */
-const makeColumnInfo = (row) => {
-    let type = 'string';
-
-    if (row.DATA_TYPE.includes('int')) type = 'number';
-    if (row.DATA_TYPE === 'timestamp' || row.DATA_TYPE === 'date' || row.DATA_TYPE === 'datetime') type = 'Date';
-    const nullOk = (row.IS_NULLABLE === 'YES') ? ' | null' : '';
-
-    if (enumToTypeMap.has(row.COLUMN_TYPE)) {
-      type = enumToTypeMap.get(row.COLUMN_TYPE);
-      enumTypeUsed.add(row.COLUMN_TYPE);
-    }
-
-    const columnType = row.COLUMN_TYPE.replace(/enum\(.+\)/, 'enum()').toLocaleUpperCase();
-    const comment = row.COLUMN_COMMENT.replace(/[\x00-\x1f]/g, ' ');
-
-    const optional = row.COLUMN_KEY ? '' : '?';
-    return `${row.COLUMN_NAME}${optional}: ${type}${nullOk};  // ${row.COLUMN_KEY} ${columnType}  ${comment}`;
-};
-
 
 /**
  * @param {Object} params
  * @param {mysql.Connection} params.conn
  * @param {string} params.database
  * @param {string|null} params.table
- * @param {Array<string>} lines
  */
-const makeTableInfo = async (params, lines) => {
-  const sql = `SELECT DISTINCT TABLE_NAME FROM COLUMNS WHERE TABLE_SCHEMA = ?`;
+const generateTS = async (params) => {
+  const enumToTypeMap = await makeEnumMap(params);
+  const tableList = await listTableName(params);
 
-  /** @type {[Array<{TABLE_NAME:string}>]} */ // @ts-ignore
-  const [res] = await params.conn.query(sql, [params.database]);
-  const tableList = res.filter((e) => { return (params.table === null || params.table === e.TABLE_NAME)}).map((e) => { return e.TABLE_NAME; });
-
+  const lines = ['/* tslint:disable */', '', `declare module '${params.database}';`, ''];
   await pMap(tableList, async (table) => {
     lines.push(`export interface ${table} {`);
-    /** @type {[Array<ColumnInfo>]} */ // @ts-ignore
-    const [res] = await params.conn.query(sqlColumns, [params.database, table]);
-    res.forEach((e) => {
-      const line = makeColumnInfo(e);
-      lines.push(`  ${line}`);
-    });
+    const columnList = await listColumn({ ...params, table });
+    columnList.forEach(e => lines.push(`  ${makeColumnInfo({ enumToTypeMap, column: e, jsdoc: false })}`));
     lines.push('}\n');
   }, {concurrency: 1});
-};
 
-
-/**
- * @param {Object} params
- * @param {mysql.Connection} params.conn
- * @param {string} params.database
- * @param {string|null} params.table
- */
-const generate = async (params) => {
-  const lines = ['/* tslint:disable */', '', `declare module '${params.database}';`, ''];
-
-  await makeEnumMap(params, lines);
-  await makeTableInfo(params, lines);
-
-  if ('dir' in options) {
+  if ('dir' in options) { // need to write file
     try { fs.mkdirSync(options.dir); } catch (err) {};  // ignore error
     fs.writeFileSync(`${options.dir}/index.js`, '');
     fs.writeFileSync(`${options.dir}/index.d.ts`, lines.join('\n'));
@@ -133,9 +51,44 @@ const generate = async (params) => {
   }
 };
 
+
+/**
+ * @param {Object} params
+ * @param {mysql.Connection} params.conn
+ * @param {string} params.database
+ * @param {string|null} params.table
+ */
+ const generateJSDoc = async (params) => {
+  const enumToTypeMap = await makeEnumMap(params);
+  const tableList = await listTableName(params);
+
+  const lines = ['// @ts-check', `// Database: ${params.database}`, ''];
+
+  await pMap(tableList, async (table) => {
+    lines.push(`/**`);
+    // lines.push(` * @memberof ${params.database}`);
+    lines.push(` * @typedef {Object} ${table}`);
+    const columnList = await listColumn({ ...params, table });
+    columnList.forEach(e => lines.push(makeColumnInfo({ enumToTypeMap, column: e, jsdoc: true })));
+    lines.push(' */\n');
+  }, {concurrency: 1});
+
+  lines.push('module.exports = {};');
+
+  if ('dir' in options) { // need to write file
+    try { fs.mkdirSync(options.dir); } catch (err) {};  // ignore error
+    fs.writeFileSync(`${options.dir}/index.js`, '');
+    fs.writeFileSync(`${options.dir}/index.d.ts`, lines.join('\n'));
+    console.log(`${options.dir}/index.d.ts, index.js  created`);
+  } else {
+    console.log(lines.join('\n'));
+  }
+};
+
+
 const main = async () => {
   if (! ('src' in options)) {
-    console.error('mysql uri is required. ex mysql://root:pass@server.com/DABASE[/TABLE]');
+    console.error('MySQL uri is required. ex mysql://root:pass@server.com/DABASE[/TABLE]');
     process.exit();
   }
   const uri = new URL(options.src);
@@ -151,9 +104,12 @@ const main = async () => {
   const requestTable = path[2] || null;
 
   let conn;
+
+  const generateFunc = ('jsdoc' in options) ? generateJSDoc : generateTS;
+
   try {
     conn = await mysql.createConnection(connectionOptions);
-    await generate({conn: conn, database: requestDatabase, table: requestTable});
+    await generateFunc({conn: conn, database: requestDatabase, table: requestTable});
   } catch (err) {
     console.error(err);
     throw err;
